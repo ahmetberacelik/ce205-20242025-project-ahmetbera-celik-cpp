c86d5868a93dbae1538656a02eb5c18ee623785e
yakup <yakup_eroglu22@erdogan.edu.tr>
2024-11-04 19:15:22 +0300

    user authentication
    bugfix and Done

--- src/userauthentication/src/userauthentication.cpp
+++ src/userauthentication/src/userauthentication.cpp
@@ -150,3 +150,291 @@ int getNewUserId(User users[], int userCount) {
 
 	return maxId + 1;
 }
+
+/**
+ * @brief Registers a new user and saves to file.
+ *
+ * @param user The user to register.
+ * @param pathFileUser The file path to save the user data.
+ * @return 1 if registration is successful, 0 otherwise.
+ */
+int registerUser(User user, const char* pathFileUser) {
+	FILE* file = fopen(pathFileUser, "rb+");
+	int userCount = 0;
+	User* users = NULL;
+
+	if (file) {
+		fread(&userCount, sizeof(int), 1, file);
+		if (userCount > 0) {
+			users = (User*)malloc(sizeof(User) * userCount);
+			fread(users, sizeof(User), userCount, file);
+
+			for (int i = 0; i < userCount; ++i) {
+				if (strcmp(users[i].email, user.email) == 0) {
+					printf("User already exists.\n");
+					fclose(file);
+					free(users);
+					enterToContinue();
+					return 0;
+				}
+			}
+		}rewind(file);
+	}
+	else { file = fopen(pathFileUser, "wb"); }
+
+	user.id = getNewUserId(users, userCount);
+	userCount++;
+	users = (User*)realloc(users, sizeof(User) * userCount);
+	users[userCount - 1] = user;
+
+	fwrite(&userCount, sizeof(int), 1, file);
+	fwrite(users, sizeof(User), userCount, file);
+
+	printf("User registered successfully\n");
+
+	if (users != NULL) {
+		free(users);
+	}
+	fclose(file);
+	enterToContinue();
+	return 1;
+}
+
+/**
+ * @brief Displays the registration menu and prompts for user information.
+ *
+ * @param pathFileUsers The file path to save the registered user data.
+ * @return 1 if registration menu is successful.
+ */
+int registerUserMenu(const char* pathFileUsers) {
+	clearScreen();
+	User newUser;
+
+	printf("Enter Name: ");
+	fgets(newUser.name, sizeof(newUser.name), stdin);
+	newUser.name[strcspn(newUser.name, "\n")] = 0;
+
+	printf("Enter Surname: ");
+	fgets(newUser.surname, sizeof(newUser.surname), stdin);
+	newUser.surname[strcspn(newUser.surname, "\n")] = 0;
+
+	printf("Enter email: ");
+	fgets(newUser.email, sizeof(newUser.email), stdin);
+	newUser.email[strcspn(newUser.email, "\n")] = 0;
+
+	printf("Enter password: ");
+	fgets(newUser.password, sizeof(newUser.password), stdin);
+	newUser.password[strcspn(newUser.password, "\n")] = 0; printf("\n"); if (registerUser(newUser, pathFileUsers)) { return 1; }
+	else { printf("Registration failed.\n"); }enterToContinue(); return 1;
+}
+
+/**
+ * @brief Attempts to log in a user with the provided credentials.
+ *
+ * @param loginUser The user attempting to log in.
+ * @param pathFileUsers The file path to read the user data from.
+ * @return 1 if login is successful, 0 otherwise.
+ */
+int loginUser(User loginUser, const char* pathFileUsers) {
+	FILE* file = fopen(pathFileUsers, "rb");
+	if (!file) {
+		printf("Failed to open user file.\n");
+		return 0;
+	}
+
+	int userCount = 0;
+	fread(&userCount, sizeof(int), 1, file);
+	if (userCount == 0) {
+		printf("No users registered.\n");
+		fclose(file);
+		enterToContinue();
+		return 0;
+	}
+
+	User userFromFile;
+	for (int i = 0; i < userCount; i++) {
+		fread(&userFromFile, sizeof(User), 1, file);
+		if (strcmp(userFromFile.email, loginUser.email) == 0 && strcmp(userFromFile.password, loginUser.password) == 0) {
+			printf("Login successful.\n");
+			fclose(file);
+			enterToContinue();
+			loggedUser = userFromFile;
+			return 1;
+		}
+	}
+
+	printf("Incorrect email or password.\n");
+	fclose(file);
+	enterToContinue();
+	return 0;
+}
+
+/**
+ * @brief Displays the login menu and prompts for user credentials.
+ *
+ * @param pathFileUsers The file path to read the user data from.
+ * @return 1 if login is successful, 0 otherwise.
+ */
+int loginUserMenu(const char* pathFileUsers) {
+	clearScreen();
+	User userLogin;
+
+	printf("Enter email: ");
+	fgets(userLogin.email, sizeof(userLogin.email), stdin);
+	userLogin.email[strcspn(userLogin.email, "\n")] = 0;
+
+	printf("Enter password: ");
+	fgets(userLogin.password, sizeof(userLogin.password), stdin);
+	userLogin.password[strcspn(userLogin.password, "\n")] = 0;
+	printf("\n");
+
+	return loginUser(userLogin, pathFileUsers);
+}
+
+/**
+ * @brief Handles guest operations menu and actions.
+ *
+ * @param pathFileIngredients The file path to the ingredients data.
+ * @param pathFileRecipes The file path to the recipes data.
+ * @return Always returns 0 when exiting guest operations.
+ */
+int guestOperations(const char* pathFileIngredients, const char* pathFileRecipes) {
+	int choice;
+
+	while (1) {
+		clearScreen();
+		printGuestMenu();
+
+		choice = getInput();
+
+		if (choice == -2) {
+			handleInputError();
+			enterToContinue();
+			continue;
+		}
+
+		switch (choice) {
+		case 1:
+			clearScreen();
+			PrintIngredientsToConsole(pathFileIngredients);
+			enterToContinue();
+			break;
+
+		case 2:
+			clearScreen();
+			printRecipesToConsole(pathFileRecipes);
+			enterToContinue();
+			break;
+
+		case 3:
+			printf("Exiting Guest Operations...\n");
+			return 0;
+
+		default:
+			printf("Invalid choice. Please try again.\n");
+			enterToContinue();
+			break;
+		}
+	}
+}
+
+/**
+ * @brief Handles user operations menu and actions.
+ *
+ * @param pathFileIngredients The file path to the ingredients data.
+ * @param pathFileRecipes The file path to the recipes data.
+ * @return Always returns 0 when exiting user operations.
+ */
+int userOperations(const char* pathFileIngredients, const char* pathFileRecipes) {
+	int choice;
+
+	while (1) {
+		printUserMenu();
+		choice = getInput();
+
+		if (choice == -2) {
+			handleInputError();
+			enterToContinue();
+			continue;
+		}
+
+		switch (choice) {
+		case 1:
+			ingredientManagementMenu("ingredient.bin");
+			break;
+
+		case 2:
+			recipeCostingMenu(pathFileIngredients, pathFileRecipes);
+			break;
+
+		case 3:
+			AdjustIngredientPriceMenu(pathFileIngredients);
+			break;
+
+		case 4:
+			budgetPlannerMenu(pathFileRecipes, pathFileIngredients);
+			break;
+
+		case 5:
+			printf("exiting");
+			return 0;
+		default:
+			clearScreen();
+			printf("Invalid choice. Please try again.\n");
+			enterToContinue();
+			break;
+		}
+	}
+}
+
+/**
+ * @brief Handles the main menu and overall program navigation.
+ *
+ * @param pathFileUsers The file path to the user data.
+ * @param pathFileIngredients The file path to the ingredients data.
+ * @param pathFileRecipes The file path to the recipes data.
+ * @return Always returns 0 when exiting the program.
+ */
+int mainMenu(const char* pathFileUsers, const char* pathFileIngredients, const char* pathFileRecipes) {
+	int choice;
+
+	while (1) {
+		clearScreen();
+		printMainMenu();
+
+		choice = getInput();
+
+		if (choice == -2) {
+			handleInputError();
+			enterToContinue();
+			continue;
+		}
+
+		switch (choice) {
+		case 1:
+			clearScreen();
+			if (loginUserMenu(pathFileUsers)) userOperations(pathFileIngredients, pathFileRecipes);
+			break;
+
+		case 2:
+			clearScreen();
+			registerUserMenu(pathFileUsers);
+			break;
+
+		case 3:
+			clearScreen();
+			guestOperations(pathFileIngredients, pathFileRecipes);
+			break;
+
+		case 4:
+			printf("Exit Program\n");
+			return 0;
+
+		default:
+			printf("Invalid choice. Please try again.\n");
+			enterToContinue();
+			break;
+		}
+	}
+
+}
