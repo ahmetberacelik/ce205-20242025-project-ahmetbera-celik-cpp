e9267843f8fe884c01f904ffbb8aaa4a77a3e0cf
Eneskoy <koy.eness@gmail.com>
2024-11-04 20:05:00 +0300

    user authentication tests
    first userauth tests writed

--- src/tests/userauthentication/userauthentication_test.cpp
+++ src/tests/userauthentication/userauthentication_test.cpp
@@ -4,19 +4,80 @@
 
 class UserauthenticationTest : public ::testing::Test {
 protected:
-
+	const char* pathFileUsers = "test_users.bin";
+	const char* pathFileIngredients = "test_ingredients.bin";
+	const char* pathFileRecieps = "test_recieps.bin";
+	const char* inputTest = "inputTest.txt";
+	const char* outputTest = "outputTest.txt";
 
 	void SetUp() override {
 		// Setup test data
 	}
 
 	void TearDown() override {
-
+		// Clean up test data
+		remove(pathFileUsers);
+		remove(pathFileIngredients);
+		remove(pathFileRecieps);
+		remove(inputTest);
+		remove(outputTest);
 	}
 
-	
+	void simulateUserInput(const char* userInput) {
+		FILE* fileinput = fopen(inputTest, "wb");
+		fputs(userInput, fileinput);
+		fclose(fileinput);
+		freopen(inputTest, "rb", stdin);
+		freopen(outputTest, "wb", stdout);
+	}
+
+	void resetStdinStdout() {
+		fclose(stdin);
+		fflush(stdout);
+#ifdef _WIN32
+		freopen("CON", "a", stdout);
+		freopen("CON", "r", stdin);
+#else
+		freopen("/dev/tty", "a", stdout);
+		freopen("/dev/tty", "r", stdin);
+#endif // _WIN32
+	}
 
 };
+
+
+TEST_F(UserauthenticationTest, enterToContinueTest) {
+	simulateUserInput("\n");
+
+	bool result = enterToContinue();
+
+	resetStdinStdout();
+
+	EXPECT_TRUE(result);
+}
+
+TEST_F(UserauthenticationTest, handleInputErrorTest) {
+	simulateUserInput("asd\n");
+
+	bool result = handleInputError();
+
+	resetStdinStdout();
+
+	EXPECT_FALSE(result);
+}
+
+TEST_F(UserauthenticationTest, getInputTest) {
+	simulateUserInput("5\n");
+	int result = getInput();
+	EXPECT_EQ(5, result);
+}
+
+TEST_F(UserauthenticationTest, getInputTest_WrongInput) {
+	simulateUserInput("asd\n");
+	int result = getInput();
+	EXPECT_EQ(-2, result);
+}
+
 
 
 
